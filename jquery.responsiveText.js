e2d9497ea345426a54dac3e053d4ad1b0676c31d
Gary Hepting <g@ryhepting.com>
2012-11-07 16:42:31 -0800

    added initial files

---
+++ jquery.responsiveText.js
@@ -0,0 +1,26 @@
+/*
+ * jQuery responsiveText - http://github.com/ghepting/responsiveText
+ * 
+ * Open source under the BSD License. 
+ * 
+ * Copyright © 2012 Gary Hepting
+ * All rights reserved.
+ */
+
+(function($) {
+	$.fn.responsiveText = function(options) {
+    	var settings = {
+			'agressiveness': options.agressiveness || 10,
+			'minSize': options.minSize || options.minSize || Number.NEGATIVE_INFINITY,
+			'maxSize': options.maxSize || options.maxSize || Number.POSITIVE_INFINITY
+		}
+		return this.each(function(){
+			var elem = $(this);
+			var resizeText = function() {
+				elem.css('font-size', Math.floor(Math.floor(Math.max(Math.min(elem.width() / (settings.agressiveness), parseFloat(settings.maxSize)), parseFloat(settings.minSize)))));
+			};
+			resizeText();
+			$(window).on('resize', resizeText);
+		});
+	}
+}(jQuery));
